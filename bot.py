a3935378c8cedba6ab61dfd3b5afd3382393227a
Glodess <dimaemec08@mail.ru>
2025-03-15 16:18:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,56 +1,54 @@
+from aiogram.client.session.aiohttp import AiohttpSession
+session = AiohttpSession(proxy='http://proxy.server:3128')
 import logging
-import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode
-from aiogram.filters import Command
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiohttp import web
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils.executor import start_webhook
 
-API_TOKEN = os.environ.get('TOKEN')
 
-WEBHOOK_HOST = 'https://your.domain'
-WEBHOOK_PATH = '/path/to/api'
+API_TOKEN = '7561819834:AAEdIF0tTVBI5QP5-y5iGMSaYiqqrN_Cp8E'
+
+proxy_url = 'http://proxy.server:3128'
+bot = Bot(token=API_TOKEN,proxy=proxy_url, session=session)
+
+WEBHOOK_HOST = 'https://www.pythonanywhere.com'
+WEBHOOK_PATH = ''
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-WEBAPP_HOST = 'localhost'
-WEBAPP_PORT = 3001
+
+WEBAPP_HOST = '0.0.0.0'  # or ip
+WEBAPP_PORT = 443
+
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN, parse_mode=ParseMode.HTML)
-dp = Dispatcher()
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
-@dp.message()
+
+@dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    return SendMessage(message.chat.id, message.text)
 
-async def on_startup(bot: Bot):
+
+async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
 
-async def on_shutdown(bot: Bot):
+
+async def on_shutdown(dp):
     logging.warning('Shutting down..')
     await bot.delete_webhook()
     logging.warning('Bye!')
 
-async def main():
-    app = web.Application()
-
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-    )
-    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-
-    app.on_startup.append(on_startup)
-    app.on_shutdown.append(on_shutdown)
-    setup_application(app, dp, bot=bot)
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, host=WEBAPP_HOST, port=WEBAPP_PORT)
-    await site.start()
-
-    await web._run_app(app)
 
 if __name__ == '__main__':
-    import asyncio
-    asyncio.run(main())
+    start_webhook(dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
